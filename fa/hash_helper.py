fd131f0f1463d84d4d1116d97bf1debd4e5af2d6
abiyar2300 <abiyar23000@gmail.com>
2025-05-26 00:12:25 +0100

    Update hash_helper.py

--- fa/hash_helper.py
+++ fa/hash_helper.py
@@ -1,2 +1,12 @@
-def validate_hash(validate_path : str) -> bool:
-    return True
+def validate_source_paths(source_paths : List[str]) -> Tuple[List[str], List[str]]:
+    valid_source_paths = []
+    invalid_source_paths = []
+
+    for source_path in source_paths:
+        if validate_hash(source_path):
+            valid_source_paths.append(source_path)
+        else:
+            invalid_source_paths.append(source_path)
+
+    return valid_source_paths, invalid_source_paths
+
